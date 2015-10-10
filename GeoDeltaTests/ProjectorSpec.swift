475dd3abbceaae7bded4093921db80d1e1936f62
Yuya Kato <yuyakato@gmail.com>
2015-10-10 15:22:01 +0900

    Projectorクラスを追加

---
+++ GeoDeltaTests/ProjectorSpec.swift
@@ -0,0 +1,13 @@
+
+import Quick
+import Nimble
+@testable import GeoDelta
+
+class ProjectorSpec : QuickSpec {
+    override func spec() {
+        describe("") {
+            it("") {
+            }
+        }
+    }
+}
