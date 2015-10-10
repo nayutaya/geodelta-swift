5860a796e7c1b4b965c86241c17e5e145d44bddb
Yuya Kato <yuyakato@gmail.com>
2015-10-10 14:49:20 +0900

    Initial Commit

---
+++ GeoDelta/GeoDelta.h
@@ -0,0 +1,19 @@
+//
+//  GeoDelta.h
+//  GeoDelta
+//
+//  Created by Yuya Kato on 2015/10/10.
+//  Copyright © 2015年 Nayutaya Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoDelta.
+FOUNDATION_EXPORT double GeoDeltaVersionNumber;
+
+//! Project version string for GeoDelta.
+FOUNDATION_EXPORT const unsigned char GeoDeltaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoDelta/PublicHeader.h>
+
+
