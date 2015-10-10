b67686c214efe9da13dbb5d0d01b8fcdc0cb8f3e
Yuya Kato <yuyakato@gmail.com>
2015-10-10 15:25:29 +0900

    geodelta-jsのコードを参考として追加

--- GeoDelta/Projector.swift
+++ GeoDelta/Projector.swift
@@ -3,3 +3,97 @@ import Foundation
 
 public class Projector {
 }
+
+/*
+var projector = {};
+
+// 度をラジアンに変換するための係数
+var DEG2RAD = Math.PI / 180.0;
+// ラジアンを度に変換するための係数
+var RAD2DEG = 180.0 / Math.PI;
+// 一辺を1.0とする正三角形の高さ
+var DELTA_HEIGHT = Math.sqrt(0.75);
+projector.DELTA_HEIGHT = function() { return DELTA_HEIGHT; };
+
+// 緯度をメルカトルY座標に変換する
+projector.latToMy = function(lat) {
+  return math.atanh(Math.sin(lat * DEG2RAD)) / Math.PI;
+};
+
+// 経度をメルカトルX座標に変換する
+projector.lngToMx = function(lng) {
+  return lng / 180.0;
+};
+
+// メルカトルY座標を緯度に変換する
+projector.myToLat = function(my) {
+  return Math.asin(math.tanh(my * Math.PI)) * RAD2DEG;
+};
+
+// メルカトルX座標を経度に変換する
+projector.mxToLng = function(mx) {
+  var x = math.mod(mx, 2.0) - 2.0;
+  if ( x < -1.0 ) {
+    x += 2.0;
+  }
+  return x * 180.0;
+};
+
+// メルカトルY座標を正規化Y座標に変換する
+projector.myToNy = function(my) {
+  return my / DELTA_HEIGHT * 12.0;
+};
+
+// メルカトルX座標を正規化X座標に変換する
+projector.mxToNx = function(mx) {
+  return mx * 12.0;
+};
+
+// 正規化Y座標をメルカトルY座標に変換する
+projector.nyToMy = function(ny) {
+  return ny / 12.0 * DELTA_HEIGHT;
+};
+
+// 正規化X座標をメルカトルX座標に変換する
+projector.nxToMx = function(nx) {
+  return nx / 12.0;
+};
+
+// 緯度を正規化Y座標に変換する
+projector.latToNy = function(lat) {
+  return projector.myToNy(projector.latToMy(lat));
+};
+
+// 経度を正規化X座標に変換する
+projector.lngToNx = function(lng) {
+  return projector.mxToNx(projector.lngToMx(lng));
+};
+
+// 正規化Y座標を緯度に変換する
+projector.nyToLat = function(ny) {
+  return projector.myToLat(projector.nyToMy(ny));
+};
+
+// 正規化X座標を経度に変換する
+projector.nxToLng = function(nx) {
+  return projector.mxToLng(projector.nxToMx(nx));
+};
+
+// 緯度経度を正規化XY座標に変換する
+projector.latLngToNxy = function(lat, lng) {
+  return {
+    nx: projector.lngToNx(lng),
+    ny: projector.latToNy(lat)
+  };
+};
+
+// 正規化XY座標を緯度経度に変換する
+projector.nxyToLatLng = function(nx, ny) {
+  return {
+    lat: projector.nyToLat(ny),
+    lng: projector.nxToLng(nx)
+  };
+};
+
+module.exports = projector;
+*/
--- GeoDeltaTests/ProjectorSpec.swift
+++ GeoDeltaTests/ProjectorSpec.swift
@@ -11,3 +11,156 @@ class ProjectorSpec : QuickSpec {
         }
     }
 }
+
+/*
+describe("projector", function() {
+  var assertRoughlyEquals = function(expected, actual, delta) {
+    assert(Math.abs(expected - actual) < delta);
+  };
+
+  describe(".latToMy", function() {
+    it("緯度をメルカトルY座標に変換する", function() {
+      assertRoughlyEquals(+1.0, projector.latToMy(+85.0511), 1e-05);
+      assertRoughlyEquals( 0.0, projector.latToMy(  0.0000), 1e-05);
+      assertRoughlyEquals(-1.0, projector.latToMy(-85.0511), 1e-05);
+    });
+  });
+
+  describe(".lngToMx", function() {
+    it("経度をメルカトルX座標に変換する", function() {
+      assertRoughlyEquals(+1.0, projector.lngToMx(+180.0), 1e-05);
+      assertRoughlyEquals(+0.5, projector.lngToMx( +90.0), 1e-05);
+      assertRoughlyEquals( 0.0, projector.lngToMx(   0.0), 1e-05);
+      assertRoughlyEquals(-0.5, projector.lngToMx( -90.0), 1e-05);
+      assertRoughlyEquals(-1.0, projector.lngToMx(-180.0), 1e-05);
+    });
+  });
+
+  describe(".myToLat", function() {
+    it("メルカトルY座標を緯度に変換する", function() {
+      assertRoughlyEquals(+85.0511, projector.myToLat(+1.0), 1e-04);
+      assertRoughlyEquals(  0.0,    projector.myToLat( 0.0), 1e-15);
+      assertRoughlyEquals(-85.0511, projector.myToLat(-1.0), 1e-04);
+    });
+  });
+
+  describe(".mxToLng", function() {
+    it("メルカトルX座標を経度に変換する", function() {
+      assertRoughlyEquals( -90.0, projector.mxToLng(+1.5), 1e-15);
+      assertRoughlyEquals(-180.0, projector.mxToLng(+1.0), 1e-15);
+      assertRoughlyEquals( +90.0, projector.mxToLng(+0.5), 1e-15);
+      assertRoughlyEquals(   0.0, projector.mxToLng( 0.0), 1e-15);
+      assertRoughlyEquals( -90.0, projector.mxToLng(-0.5), 1e-15);
+      assertRoughlyEquals(-180.0, projector.mxToLng(-1.0), 1e-15);
+      assertRoughlyEquals( +90.0, projector.mxToLng(-1.5), 1e-15);
+    });
+  });
+
+  describe(".myToNy", function() {
+    it("メルカトルY座標を正規化Y座標に変換する", function() {
+      var max = projector.DELTA_HEIGHT();
+      assertRoughlyEquals(+12.0, projector.myToNy(+max), 1e-15);
+      assertRoughlyEquals(  0.0, projector.myToNy( 0.0), 1e-15);
+      assertRoughlyEquals(-12.0, projector.myToNy(-max), 1e-15);
+    });
+  });
+
+  describe(".mxToNx", function() {
+    it("メルカトルX座標を正規化X座標に変換する", function() {
+      assertRoughlyEquals(+12.0, projector.mxToNx(+1.0), 1e-15);
+      assertRoughlyEquals(  0.0, projector.mxToNx( 0.0), 1e-15);
+      assertRoughlyEquals(-12.0, projector.mxToNx(-1.0), 1e-15);
+    });
+  });
+
+  describe(".nyToMy", function() {
+    it("正規化Y座標をメルカトルY座標に変換する", function() {
+      var max = projector.DELTA_HEIGHT();
+      assertRoughlyEquals(+max, projector.nyToMy(+12.0), 1e-15);
+      assertRoughlyEquals( 0.0, projector.nyToMy(  0.0), 1e-15);
+      assertRoughlyEquals(-max, projector.nyToMy(-12.0), 1e-15);
+    });
+  });
+
+  describe(".nxToMx", function() {
+    it("正規化X座標をメルカトルX座標に変換する", function() {
+      assertRoughlyEquals(+1.0, projector.nxToMx(+12.0), 1e-15);
+      assertRoughlyEquals( 0.0, projector.nxToMx(  0.0), 1e-15);
+      assertRoughlyEquals(-1.0, projector.nxToMx(-12.0), 1e-15);
+    });
+  });
+
+  describe(".latToNy", function() {
+    it("緯度を正規化Y座標に変換する", function() {
+      assertRoughlyEquals(projector.myToNy(projector.latToMy(+82.4674)), projector.latToNy(+82.4674), 1e-15);
+      assertRoughlyEquals(                                          0.0, projector.latToNy(  0.0   ), 1e-15);
+      assertRoughlyEquals(projector.myToNy(projector.latToMy(-82.4674)), projector.latToNy(-82.4674), 1e-15);
+    });
+  });
+
+  describe(".lngToNx", function() {
+    it("経度を正規化X座標に変換する", function() {
+      assertRoughlyEquals(projector.mxToNx(projector.lngToMx(+180.0)), projector.lngToNx(+180.0), 1e-15);
+      assertRoughlyEquals(                                        0.0, projector.lngToNx(   0.0), 1e-15);
+      assertRoughlyEquals(projector.mxToNx(projector.lngToMx(-180.0)), projector.lngToNx(-180.0), 1e-15);
+    });
+  });
+
+  describe(".nyToLat", function() {
+    it("正規化Y座標を緯度に変換する", function() {
+      assertRoughlyEquals(projector.myToLat(projector.nyToMy(+12.0)), projector.nyToLat(+12.0), 1e-15);
+      assertRoughlyEquals(                                       0.0, projector.nyToLat(  0.0), 1e-15);
+      assertRoughlyEquals(projector.myToLat(projector.nyToMy(-12.0)), projector.nyToLat(-12.0), 1e-15);
+    });
+  });
+
+  describe(".nxToMx", function() {
+    it("正規化X座標を経度に変換する", function() {
+      assertRoughlyEquals(projector.mxToLng(projector.nxToMx(+12.0)), projector.nxToLng(+12.0), 1e-15);
+      assertRoughlyEquals(                                       0.0, projector.nxToLng(  0.0), 1e-15);
+      assertRoughlyEquals(projector.mxToLng(projector.nxToMx(-12.0)), projector.nxToLng(-12.0), 1e-15);
+    });
+  });
+
+  describe(".latLngToNxy", function() {
+    it("緯度経度を正規化XY座標に変換する", function() {
+      assertRoughlyEquals(projector.lngToNx(+180.0000), projector.latLngToNxy(+82.4674, +180.0).nx, 1e-15);
+      assertRoughlyEquals(projector.latToNy( +82.4674), projector.latLngToNxy(+82.4674, +180.0).ny, 1e-15);
+      assertRoughlyEquals(0.0, projector.latLngToNxy(0.0, 0.0).nx, 1e-15);
+      assertRoughlyEquals(0.0, projector.latLngToNxy(0.0, 0.0).ny, 1e-15);
+      assertRoughlyEquals(projector.lngToNx(-180.0000), projector.latLngToNxy(-82.4674, -180.0).nx, 1e-15);
+      assertRoughlyEquals(projector.latToNy( -82.4674), projector.latLngToNxy(-82.4674, -180.0).ny, 1e-15);
+    });
+  });
+
+  describe(".nxyToLatLng", function() {
+    it("正規化XY座標を緯度経度に変換する", function() {
+      assertRoughlyEquals(projector.nyToLat(+12.0), projector.nxyToLatLng(+12.0, +12.0).lat, 1e-15);
+      assertRoughlyEquals(projector.nxToLng(+12.0), projector.nxyToLatLng(+12.0, +12.0).lng, 1e-15);
+      assertRoughlyEquals(0.0, projector.nxyToLatLng(0.0, 0.0).lat, 1e-15);
+      assertRoughlyEquals(0.0, projector.nxyToLatLng(0.0, 0.0).lng, 1e-15);
+      assertRoughlyEquals(projector.nyToLat(-12.0), projector.nxyToLatLng(-12.0, -12.0).lat, 1e-15);
+      assertRoughlyEquals(projector.nxToLng(-12.0), projector.nxyToLatLng(-12.0, -12.0).lng, 1e-15);
+    });
+  });
+
+TODO: function test_randomLatLngAndNxy()
+{
+  final double latMax = geodelta.projector.nyToLat(+12.0);
+  final double lngMax = 180.0;
+  final Random r = new Random();
+  for ( int i = 0; i < 1000; i++ )
+  {
+    final double lat1 = r.nextDouble() * latMax * 2 - latMax;
+    final double lng1 = r.nextDouble() * lngMax * 2 - lngMax;
+    final double nx = geodelta.projector.lngToNx(lng1);
+    final double ny = geodelta.projector.latToNy(lat1);
+    final double lat2 = geodelta.projector.nyToLat(ny);
+    final double lng2 = geodelta.projector.nxToLng(nx);
+    assertRoughlyEquals(lat1, lat2, 1E-13);
+    assertRoughlyEquals(lng1, lng2, 1E-13);
+  }
+}
+
+});
+*/
