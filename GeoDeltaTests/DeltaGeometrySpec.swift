c08fe78bae89515bf6be4499ae45e863ba257461
Yuya Kato <yuyakato@gmail.com>
2015-10-14 13:08:43 +0900

    DeltaGeometryクラスのひな形を追加

---
+++ GeoDeltaTests/DeltaGeometrySpec.swift
@@ -0,0 +1,13 @@
+
+import Quick
+import Nimble
+@testable import GeoDelta
+
+class DeltaGeometrySpec : QuickSpec {
+    override func spec() {
+        describe("") {
+            it("") {
+            }
+        }
+    }
+}
