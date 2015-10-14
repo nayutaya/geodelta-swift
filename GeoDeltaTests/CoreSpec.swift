fa66aac1a57f69da8ae38aef96cbc111d6ae40e2
Yuya Kato <yuyakato@gmail.com>
2015-10-14 16:08:42 +0900

    Coreクラスのひな形を追加

---
+++ GeoDeltaTests/CoreSpec.swift
@@ -0,0 +1,13 @@
+
+import Quick
+import Nimble
+@testable import GeoDelta
+
+class CoreSpec : QuickSpec {
+    override func spec() {
+        describe("") {
+            it("") {
+            }
+        }
+    }
+}
